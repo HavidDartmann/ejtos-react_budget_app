9dcc9d6bdd1bda97b2cb3d28892d17b3599ca919
David <david.comdh80@gmail.com>
2024-03-27 13:36:04 -0400

    DD

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, dispatch } = useContext(AppContext);
+    const { budget, currency, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
@@ -12,10 +12,10 @@ const Budget = () => {
         });
     }
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget} </span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+        <span>Budget: {currency} </span>
+        <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
     );
 };
 export default Budget;
---
+++ src/components/Currency.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Currency = () => {
+    const { dispatch, currency } = useContext(AppContext);
+
+    const handleCurrencyChange = (event) => {
+        console.log(event);
+        const newCurrency = event;
+        dispatch({ type: 'CHG_CURRENCY', payload: newCurrency });
+    };
+
+    return (
+        <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Currency ({currency})
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="currencyDropdown">
+                <li><a className="dropdown-item" onClick={() => handleCurrencyChange('$')}>$ Dollar</a></li>
+                <li><a className="dropdown-item" onClick={() => handleCurrencyChange('£')}>£ Pound</a></li>
+                <li><a className="dropdown-item" onClick={() => handleCurrencyChange('€')}>€ Euro</a></li>
+                <li><a className="dropdown-item" onClick={() => handleCurrencyChange('₹')}>₹ Rupee</a></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Currency;
+
+
+
